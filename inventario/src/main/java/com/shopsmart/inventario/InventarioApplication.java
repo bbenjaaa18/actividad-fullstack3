c356c5a362595064379c37575b1967f5e76792ea
bbenjaaa18 <benjamella926@gmail.com>
2026-03-11 14:05:08 -0300

    fix dockerfile java image

--- inventario/src/main/java/com/shopsmart/inventario/InventarioApplication.java
+++ inventario/src/main/java/com/shopsmart/inventario/InventarioApplication.java
@@ -2,7 +2,7 @@ package com.shopsmart.inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
+//git
 
 @SpringBootApplication
 public class InventarioApplication {
